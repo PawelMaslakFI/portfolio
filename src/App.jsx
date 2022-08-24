f9db8ee62c2658d60b3dd44b099ae09314353e2e
krychkarol <krych.karol.93@gmail.com>
2022-08-24 18:06:26 +0200

    add loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Background from "./components/Background";
+import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className="App">
+            <Loader/>
             <Navbar/>
             <Background/>
         </div>
