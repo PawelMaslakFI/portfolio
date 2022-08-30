c8c4b1fdeac8611abfca788374bc627a0523665c
krychkarol <krych.karol.93@gmail.com>
2022-08-30 15:27:50 +0200

    home page v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="home">
-            HOME PAGE
+            <div className="wrapper">
+                <div className="content">
+                    <p className="p1">
+                        Hello, my name is
+                    </p>
+                    <p className="p2">
+                        Karol Krych.
+                    </p>
+                    <p className="p3">
+                        I'm a web developer.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
